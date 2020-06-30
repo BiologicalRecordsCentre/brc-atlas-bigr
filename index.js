ed1eda920a1554e132b57d8e0a18066fc4fa9a6a
Rich Burkmar <rich.burkmar@gmail.com>
2020-07-01 00:25:30 +0100

    Moved main JS source files into new src folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { getCentroid } from './getCentroid'
-import { transformFunction,  widthFromHeight, namedTransOpts } from './coordsToImage'
+import { getCentroid } from './src/getCentroid'
+import { transformFunction,  widthFromHeight, namedTransOpts } from './src/coordsToImage'
 
 export { getCentroid }
 export { transformFunction,  widthFromHeight, namedTransOpts } 
