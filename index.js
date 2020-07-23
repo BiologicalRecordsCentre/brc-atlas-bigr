e969383217b32f7b61b743b1ea5c98a1b45ae84f
Richard Burkmar <rburkmar@ceh.ac.uk>
2020-07-23 09:16:13 +0100

    Updated exports and version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { getCentroid } from './src/getCentroid'
+import { getGjson } from './src/getGjson'
+import { checkGr } from './src/checkGr'
 import { getTweenTransOpts, getInsetDims, transformFunction,  widthFromHeight, namedTransOpts } from './src/coordsToImage'
 
-export { getCentroid }
+export { getCentroid, checkGr, getGjson }
 export { getTweenTransOpts, getInsetDims, transformFunction,  widthFromHeight, namedTransOpts } 
