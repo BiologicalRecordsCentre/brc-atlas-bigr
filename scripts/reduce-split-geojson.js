9600c10d6a32bae5b2b9d5cdec22f5655220d01d
Richard Burkmar <rburkmar@ceh.ac.uk>
2021-11-11 11:21:27 +0000

    Added MBR file generation to geojson split script

--- scripts/reduce-split-geojson.js
+++ scripts/reduce-split-geojson.js
@@ -13,6 +13,9 @@
 // a prefix which will be appended to the attribute value - set to an empty
 // string if you don't want to add a prefix
 //
+// The script also produces and extra output file called mbrs.csv which
+// records the minumum bounding rectangles of each of the output geojson files.
+//
 //Use like this:
 // node reduce-split-geojson.js <input-file.geojson> <output-folder> <decimal places> <attribute> <prefix>
 
@@ -25,21 +28,64 @@ if (process.argv.length !== 7) {
     process.exit(1)
 }
 
+function round(n) {
+  return Math.round(n * Math.pow(10, process.argv[4])) / Math.pow(10, process.argv[4])
+}
+
 function roundCoords(arr) {
   // Iterate over the array
   return arr.map(function(e) {
     // If the element is an array call the function recursively
     // otherwise round the numbers
-    return Array.isArray(e) ? roundCoords(e) : Math.round(e * Math.pow(10, process.argv[4])) / Math.pow(10, process.argv[4])
+    return Array.isArray(e) ? roundCoords(e) : round(e)
+  })
+}
+
+function updateMbr(arr, mbr) {
+  // Iterate over the array
+  arr.map(function(e) {
+    // If the element is an array call the function recursively
+    // otherwise update the MBR if necessary
+    if (!Array.isArray(e[0])) {
+      const lat = e[1]
+      const lon = e[0]
+      mbr.ll.lon = lon < mbr.ll.lon ? lon : mbr.ll.lon
+      mbr.ll.lat = lat < mbr.ll.lat ? lat : mbr.ll.lat
+      mbr.ur.lon = lon > mbr.ur.lon ? lon : mbr.ur.lon
+      mbr.ur.lat = lat > mbr.ur.lat ? lat : mbr.ur.lat
+    } else {
+      updateMbr(e, mbr) 
+    }
   })
 }
 
 if (fse.existsSync(process.argv[2])) {
   if (fse.existsSync(process.argv[3])) {
+
+    // Write header for MBR file
+    fse.writeFileSync(process.argv[3] + '/mbrs.csv', 'vc, lllat, lllon, urlat, urlon\n')
+
+    // Process the features
     let input = fse.readFileSync(process.argv[2])
     let json = JSON.parse(input)
     const newFeatures = json.features.map(function(f){
-      // Does not output any feature properties
+
+      // Make mbr calculation
+      const mbr = {
+        ll: {
+          lat: 90,
+          lon: 90
+        },
+        ur: {
+          lat: 0,
+          lon: -90
+        }
+      }
+      updateMbr(f.geometry.coordinates, mbr)
+      // Write mbr into file`
+      fse.appendFileSync(process.argv[3] + '/mbrs.csv', `${process.argv[6] + f.properties[process.argv[5]]},${round(mbr.ll.lat)},${round(mbr.ll.lon)},${round(mbr.ur.lat)},${round(mbr.ur.lon)}\n`)
+
+      // Create output feature - does not output any feature properties
       const fNew =  {
         type: f.type,
         filename: process.argv[6] + f.properties[process.argv[5]] + '.geojson',
@@ -50,6 +96,7 @@ if (fse.existsSync(process.argv[2])) {
       }
       return fNew
     })
+
     newFeatures.forEach(function(f) {
       const filename = f.filename
       delete f.filename
