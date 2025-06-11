a63c553d0af0e1bb496dd06c2ec9fdced502dc68
Jim Bacon <jame2@ceh.ac.uk>
2025-06-11 16:08:30 +0000

    Fix npm build failure
    Import assertions are replaced by attributes.
    Assert is no longer allowed in Node >= v22.
    With is allowed in Node >= v16.14

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { babel } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
-import pkg from './package.json' assert { type: "json" }
+import pkg from './package.json' with { type: "json" }
 
 export default [
 	// browser-friendly UMD builds
